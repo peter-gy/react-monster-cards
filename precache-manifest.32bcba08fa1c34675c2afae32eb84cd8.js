de5d5936c00f924e513a268500da35c7d52fd0e6
Péter Gyarmati <gyarmatipeti01@gmail.com>
2020-04-29 10:51:01 +0200

    Updates

---
+++ precache-manifest.32bcba08fa1c34675c2afae32eb84cd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76d5f351c6b54d4e6914d7422ca1bb43",
+    "url": "/react-monster-cards/index.html"
+  },
+  {
+    "revision": "e847e52e2a0c1cbaa61a",
+    "url": "/react-monster-cards/static/css/main.75fa479d.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/react-monster-cards/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monster-cards/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e847e52e2a0c1cbaa61a",
+    "url": "/react-monster-cards/static/js/main.0968a0ee.chunk.js"
+  },
+  {
+    "revision": "1694ba8b8164eba3d056",
+    "url": "/react-monster-cards/static/js/runtime-main.d5d1d58f.js"
+  }
+]);
